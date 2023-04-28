3da76c07d8c4bbeece388ea893d642d432a1e208
Natalia P <125387029+natalias-code@users.noreply.github.com>
2023-04-28 16:07:36 +0100

    Update ScooterApp.js

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,6 +3,25 @@ const Scooter = require('./Scooter')
 
 class ScooterApp {
   // ScooterApp code here
+  constructor()
+  {
+    this.stations = [];
+    this.registeredUsers = [];
+  }
+
+  createScooter(stationName) {
+    const station = this.stations.find(station => station.name === stationName);
+    if (!station) {
+      console.log(`Station ${stationName} does not exist.`);
+      return false;
+    } else {
+      const scooter = new Scooter();
+      station.addScooter(scooter);
+      console.log(`Scooter created successfully at station ${stationName}.`);
+      return scooter;
+    }
+  }
+
 }
 
 module.exports = ScooterApp
